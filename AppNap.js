18adf165af072cbee592251f3094bb1520aac975
Petter Kjelkenes <kjelkenes@gmail.com>
2021-05-19 10:08:11 +0200

    Remove bindings node helper. It does not work with most native-app guis.

--- AppNap.js
+++ AppNap.js
@@ -1,13 +1,16 @@
-const addon = require('bindings')('AppNap.node')
+const addon = require('./build/Release/AppNap.node')
 
-module.exports = function (reason = '') {
-  if (process.platform !== 'darwin') {
-    throw new Error('System is not macOS')
+module.exports = {
+  disable: function (reason = '') {
+    if (process.platform !== 'darwin') {
+      throw new Error('System is not macOS')
+    }
+
+    if (!reason || reason.length === 0 || typeof reason !== 'string') {
+      throw new Error('Argument of type string expected')
+    }
+
+    return addon.disable(reason)
   }
 
-  if (!reason || reason.length === 0 || typeof reason !== 'string') {
-    throw new Error('Argument of type string expected')
-  }
-
-  return addon.disable(reason)
 }
